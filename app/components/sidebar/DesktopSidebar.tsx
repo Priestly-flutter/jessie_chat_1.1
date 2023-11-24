64f525a107df7c6cb66de1f81fd2371fc92aafb6
JatoPriestlyNjilah <priestlynjilah@gmail.com>
2023-11-25 00:28:36 +0100

    worked on icons

---
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+
+const DesktopSidebar = () =>{
+    const routes = useRoutes();
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    return(
+        <div className="
+            hidden
+            lg:fixed
+            lg:inset-y-0
+            lg:left-0
+            lg:z-40
+            lg:w-20
+            xl:px-6
+            lg:overflow-y-auto
+            lg:bg-white
+            lg:border-r[10px]
+            lg:pb-4
+            lg:flex
+            lg:flex-col
+            justify-between
+        ">
+            hello
+        </div>
+    );
+}
+
+
+export default DesktopSidebar; 
+//took a break 
---
+++ app/hooks/useRoutes.ts
@@ -0,0 +1,39 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+//run npm install react-icons to install react icon 
+
+
+const useRoutes = () => {
+    const pathname = usePathname();
+
+    const { conversationId } = useConversation();
+
+    const routes = useMemo(() => [
+        {
+            label: 'Chat',
+            href: '/conversation',
+            icon: HiChat,
+            active: pathname === '/conversation' || !!conversationId
+        },
+        {
+            label: 'Users',
+            href: '/users',
+            icon: HiUsers,
+            active: pathname === '/users'
+        },
+        {
+            label: 'Logout',
+            href: '#',
+            icon: HiArrowLeftOnRectangle,
+            onClick: () => signOut()
+        },                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    ],[pathname,conversationId])
+
+    return routes;
+}
+
+export default useRoutes;
